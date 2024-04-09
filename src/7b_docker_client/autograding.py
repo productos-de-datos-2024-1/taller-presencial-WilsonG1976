21c63ad92ea7019faf0f0e1b34482da81702fc29
WILSON DARIO GRACIANO BRAVO <Wilson.Graciano@epm.com.co>
2024-04-09 18:12:53 -0500

    Taller1_v4

--- src/7b_docker_client/autograding.py
+++ src/7b_docker_client/autograding.py
@@ -5,7 +5,7 @@ import os
 # test code files
 assert os.path.exists("src/7b_docker_client/config.json")
 assert os.path.exists("src/7b_docker_client/app.py")
-assert os.path.exists("src/7b_docker_client/dockerfile")
+assert os.path.exists("src/7b_docker_client/Dockerfile")
 assert os.path.exists("src/7b_docker_client/templates/index.html")
 
 
